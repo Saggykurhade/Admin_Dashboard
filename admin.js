c14d2a044a75904eef8c36397671576c86e57f95
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2024-02-16 20:12:47 +0530

    updates

--- admin.js
+++ admin.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
 
+    const menu = document.querySelector('.menu-btn');
+    const sidebar = document.querySelector('.sidebar');
+
+    menu.addEventListener('click', function () {
+        sidebar.classList.toggle('active');
+        if (window.innerWidth <= 768) {
+            sidebar.classList.remove('active');
+        }
+    });
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if(scroll > 50) {
+            $('.navbar').css('background', '#101545');
+            $('.navbar').css('box-shadow', "rgba(0, 0, 0, 0.1) 0px 4px 12px");
+        } else {
+            $('.navbar').css('background', 'transparent');
+            $('.navbar').css('box-shadow', "none");
+        }
+    })
+
     var options = {
         series: [{
             name: 'series1',
